a0c26be5a1555e1ab9d509d911c80dabcb74ac82
cgvak <cgvak@CGVAK-PC448>
2024-09-30 11:49:40 +0530

    Add project files.

---
+++ ToDoListAPP/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoListAPP.Models
+{
+    public class TodoItem
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
